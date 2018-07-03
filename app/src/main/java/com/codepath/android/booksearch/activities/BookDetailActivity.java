7d3db107e20763fc38cd9e2f1319d1f6dfa783af
Christine Wang <christinewang@fb.com>
2018-07-03 11:03:54 -0700

    Added search bar and details view

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -7,12 +7,18 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.codepath.android.booksearch.GlideApp;
 import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.models.Book;
+
+import org.parceler.Parcels;
 
 public class BookDetailActivity extends AppCompatActivity {
     private ImageView ivBookCover;
     private TextView tvTitle;
     private TextView tvAuthor;
+
+    Book book;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +30,14 @@ public class BookDetailActivity extends AppCompatActivity {
         tvAuthor = (TextView) findViewById(R.id.tvAuthor);
 
         // Extract book object from intent extras
+        book = Parcels.unwrap(getIntent().getParcelableExtra(Book.class.getSimpleName()));
 
         // Use book object to populate data into views
+        tvTitle.setText(book.getTitle());
+        tvAuthor.setText(book.getAuthor());
+        GlideApp.with(this)
+                .load(book.getCoverUrl())
+                .into(ivBookCover);
     }
 
 
